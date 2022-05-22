44781348f924b3735ae761fa3d2f520350bde91e
Digoassun <digoassun@gmail.com>
2022-05-22 20:36:26 -0300

    Alterações no js

--- js/controllers/controllerCep.js
+++ js/controllers/controllerCep.js
@@ -1,15 +1,16 @@
-class CepController{
-    passaCep(){
+class CepController {
+    passaCep() {
         const validacao = new CepModel($('#cep').val())
         const mostra = new CepView()
+
         validacao.reqCEP()
+
         mostra.mostraReq(validacao)
-        
     }
 }
 
 const controller = new CepController()
-$('#buscaCep').click((e)=>{
+$('#buscaCep').click((e) => {
     e.preventDefault()
     controller.passaCep()
 })
--- js/models/modelCep.js
+++ js/models/modelCep.js
@@ -1,55 +1,36 @@
 class CepModel {
-    constructor(cep){
-        this.estado='';
-        this.cidade='';
-        this.rua='';
-        this.bairro='';
-        this.cep=$('#cep');
+    constructor() {
+        this.estado = '';
+        this.cidade = '';
+        this.rua = '';
+        this.bairro = '';
+        this.cep = $('#cep');
     }
 
     reqCEP() {
         const requisicao = new XMLHttpRequest()
 
-        requisicao.addEventListener('load',()=>{
-            if(requisicao.status==200){
+        requisicao.addEventListener('load', () => {
+            if (requisicao.status == 200) {
                 const dados = this._processaResposta(requisicao.responseText)
                 this._att(dados)
+            } else if(requisicao.status ==400){
+                throw new Error('Cep invalido')
             }
         })
-        requisicao.open('GET', `https://viacep.com.br/ws/${this.cep.val()}/json/`, false)       
+        requisicao.open('GET', `https://viacep.com.br/ws/${this.cep.val()}/json/`, false)
         requisicao.send()
     }
 
-    _processaResposta(respostaString){
+    _processaResposta(respostaString) {
         const resposta = JSON.parse(respostaString)
         return resposta
     }
 
-    _att(dados){
+    _att(dados) {
         this.estado = dados.uf
         this.cidade = dados.localidade
         this.bairro = dados.bairro
         this.rua = dados.logradouro
     }
-
-
 }
-
-
-
-// $("#buscaCep").click((e) => {
-//     e.preventDefault()
-//     const cepInput = $('#cep').val()
-//     $.ajax({
-//         url: `https://viacep.com.br/ws/${cepInput}/json/`,
-//         type: "GET",
-//         success: function (data) {
-//             console.log(data)
-
-//             $('#estado').val(data.uf)
-//             $("#cidade").val(data.localidade)
-//             $("#bairro").val(data.bairro)
-//             $("#rua").val(data.logradouro)
-//         }
-//     })
-// })
