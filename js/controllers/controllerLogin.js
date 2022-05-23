05660e3d44b0903309803c416782ac9849c213a9
Talyssonbs <talyssonbs@gmail.com>
2022-05-23 18:39:34 -0300

    Mensagens e imagens para validação

--- js/controllers/controllerLogin.js
+++ js/controllers/controllerLogin.js
@@ -1,2 +1,25 @@
-const email = document.getElementById('email')
-const password = document.getElementById('senha')
+var email = document.getElementById('email')
+var password = document.getElementById('senha')
+
+/* var email = prompt("Qual é o seu email?"); // resilia@resilia  */
+
+var respostaValidacao = validaEmail(email);
+console.log(respostaValidacao);
+ExibeResultadoValidacao(respostaValidacao);
+
+function validaEmail(email) {
+    var posicao = email.indexOf("@");
+    if (posicao > -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ExibeResultadoValidacao(resultado) {
+    if (resultado) {
+        document.write("email validado");
+    } else {
+        document.write("email fornecido INVALIDO");
+    }
+}
