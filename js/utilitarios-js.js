3d7694fec696444b74961faaa72e590dea0282cf
Willian Carvalho <wcarvalho91610@gmail.com>
2022-05-24 13:44:16 -0300

    criei os eventos no js da pag cadastro e aumentei o tamanho dos inputs

---
+++ js/utilitarios-js.js
@@ -0,0 +1,25 @@
+const home = document.querySelector('#home');
+const servico = document.getElementById('servicos');
+const contato = document.getElementById('contato');
+const login = document.getElementById('login');
+const cadastro = document.getElementById('cadastro')
+
+home.addEventListener("click", ()=>{
+    location.assign("../index.html");
+})
+
+servico.addEventListener("click", ()=>{
+    location.assign("./servicos.html");
+})
+
+contato.addEventListener("click", ()=>{
+    location.assign("./contatos.html");
+})
+
+login.addEventListener("click", ()=>{
+    location.assign("./login.html");
+})
+
+cadastro.addEvent("click", ()=>{
+    location.assign("./cadastro.html");
+})
