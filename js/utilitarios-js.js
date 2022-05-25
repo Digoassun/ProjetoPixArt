5ab208e448b89a3f0b9a08840a97eb6c7b342fd2
Willian Carvalho <wcarvalho91610@gmail.com>
2022-05-24 23:07:54 -0300

    js identado

--- js/utilitarios-js.js
+++ js/utilitarios-js.js
@@ -1,25 +1,27 @@
-const home = document.querySelector('#home');
-const servico = document.getElementById('servicos');
-const contato = document.getElementById('contato');
-const login = document.getElementById('login');
-const cadastro = document.getElementById('cadastro')
+function redirecionaCadastro() {
+    const home = document.querySelector('#home');
+    const servico = document.getElementById('servicos');
+    const contato = document.getElementById('contato');
+    const login = document.getElementById('login');
+    const cadastro = document.getElementById('cadastro')
 
-home.addEventListener("click", ()=>{
-    location.assign("../index.html");
-})
+    home.addEventListener("click", () => {
+        location.assign("../index.html");
+    })
 
-servico.addEventListener("click", ()=>{
-    location.assign("./servicos.html");
-})
+    servico.addEventListener("click", () => {
+        location.assign("./servicos.html");
+    })
 
-contato.addEventListener("click", ()=>{
-    location.assign("./contatos.html");
-})
+    contato.addEventListener("click", () => {
+        location.assign("./contatos.html");
+    })
 
-login.addEventListener("click", ()=>{
-    location.assign("./login.html");
-})
+    login.addEventListener("click", () => {
+        location.assign("./login.html");
+    })
 
-cadastro.addEventListener("click", ()=>{
-    location.assign("./cadastro.html");
-})
+    cadastro.addEventListener("click", () => {
+        location.assign("./cadastro.html");
+    })
+}
