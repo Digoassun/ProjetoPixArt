8b330465c92b2f5e36a985e26931e0dba7039e43
Talyssonbs <talyssonbs@gmail.com>
2022-05-26 00:26:36 -0300

    Modificações nos cards, banners, descrições e redirecionamento dos botões

--- js/utilitarios-js.js
+++ js/utilitarios-js.js
@@ -3,23 +3,36 @@ const servico = document.getElementById('servicos');
 const contato = document.getElementById('contato');
 const login = document.getElementById('login');
 const cadastro = document.getElementById('cadastro')
+const btnServicos = document.querySelectorAll('.btn-servicos');
 
-home.addEventListener("click", ()=>{
+home.addEventListener("click", () => {
     location.assign("../index.html");
 })
 
-servico.addEventListener("click", ()=>{
+servico.addEventListener("click", () => {
     location.assign("./servicos.html");
 })
 
-contato.addEventListener("click", ()=>{
+contato.addEventListener("click", () => {
     location.assign("./contatos.html");
 })
 
-login.addEventListener("click", ()=>{
+login.addEventListener("click", () => {
     location.assign("./login.html");
 })
 
-cadastro.addEventListener("click", ()=>{
+cadastro.addEventListener("click", () => {
+    location.assign("./cadastro.html");
+})
+
+btnServicos[0].addEventListener("click", () => {
+    location.assign("./cadastro.html");
+})
+
+btnServicos[1].addEventListener("click", () => {
+    location.assign("./cadastro.html");
+})
+
+btnServicos[2].addEventListener("click", () => {
     location.assign("./cadastro.html");
 })
